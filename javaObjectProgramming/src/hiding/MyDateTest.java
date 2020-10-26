4c7e196d847037526c487c78b5ff4af204bc6db2
coolyul <coolyul92@gmail.com>
2020-10-26 16:19:27 +0900

    java 객체지향 프로그래밍

---
+++ javaObjectProgramming/src/hiding/MyDateTest.java
@@ -0,0 +1,17 @@
+package hiding;
+
+public class MyDateTest {
+
+	public static void main(String[] args) {
+		
+		MyDate mydate = new MyDate();
+		
+		mydate.setYear(2020);
+		mydate.setMonth(10);
+		mydate.setDay(22);
+		mydate.showDate();
+		
+
+	}
+
+}
